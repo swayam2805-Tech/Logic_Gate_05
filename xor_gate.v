1a7aaad6968551def1cc6e01b2012b216c54ca30
swayam2805-Tech <swayampratyush2805@gmail.com>
2026-01-28 11:01:35 +0530

    Add XOR gate module in Verilog

---
+++ xor_gate.v
@@ -0,0 +1,5 @@
+module xor_gate(output c, input a, b);
+    // Instantiate the built-in XOR gate primitive
+    // Format: xor instance_name (output, input1, input2, ...);
+    xor (c, a, b); 
+endmodule
